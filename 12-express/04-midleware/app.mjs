1c2459af8b9ea3ce83dde2b73f171b948103bd58
Alex <marketdoski@gmail.com>
2023-11-26 02:06:20 +0200

    express midlewares

---
+++ 12-express/04-midleware/app.mjs
@@ -0,0 +1,17 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res) => {
+    console.log(req.body);
+    return res.send('This is express server');
+});
+
+app.listen(5000, () => {
+    console.log('Server running on port 5000');
+});
